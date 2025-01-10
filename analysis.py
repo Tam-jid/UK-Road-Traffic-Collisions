a4c6df5d4241ead5fd4ca00ae2ff8c4f0bc051bf
Tamjid Choudhury <tamjid2005@gmail.com>
2025-01-10 03:17:54 +0000

    Removed data cleaning from test suite

--- analysis.py
+++ analysis.py
@@ -2,5 +2,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
-data = pd.read_csv('filtered_accident_data.csv')
+
+data = pd.read_csv('Dataset/filtered_accident_data.csv')
 print(data.head())
